6b72055efb43f666e2ca46daa0e281438bb6462a
Andre Menezes <dehhmenezes87@gmail.com>
2024-07-15 09:48:40 -0300

    feat: botão fixo whatsapp

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,11 @@
     <AboutMe />
     <AboutSessions />
     <TerapySection />
+    <a class="fixed bottom-8 right-8 rounded-full z-20 w-14 h-14 bg-green-600 hover:bg-green-600/90 flex justify-center items-center"
+      :href="whatsappLink">
+      <mdi-icon type="mdi" :path="mdiWhatsapp" class="w-10 h-10  text-white">
+      </mdi-icon>
+    </a>
   </main>
 </template>
 
@@ -16,4 +21,6 @@ import HeroBanner from './components/HeroBanner.vue';
 import NavBar from './components/NavBar.vue';
 import IntroduceSection from './components/IntroduceSection.vue';
 import TerapySection from './components/TerapySection.vue';
+import { mdiWhatsapp } from '@mdi/js';
+import { whatsappLink } from './utils/constants';
 </script>
