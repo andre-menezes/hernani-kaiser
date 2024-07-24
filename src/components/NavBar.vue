30fa753e5343b3d8df33707c7673b6452f357084
Andre Menezes <dehhmenezes87@gmail.com>
2024-07-24 09:19:03 -0300

    fix: navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav class="w-full h-fit py-4 px-8 sm:px-20 fixed bg-primary shadow z-50">
-    <ul class="flex gap-4">
+  <nav class="w-full h-fit py-4 px-8 sm:px-20 text-xs sm:text-lg fixed bg-primary shadow z-50">
+    <ul class="flex gap-4 sm:gap-8">
       <li class="cursor-pointer"><a class="uppercase font-semibold" @click="scrollToSection('top', '/')">Início</a></li>
       <li class="cursor-pointer"><a class="uppercase font-semibold"
           @click="scrollToSection('sobre-mim', '/sobre-mim')">Sobre mim</a>
